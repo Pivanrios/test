89fbb20c4d6bdf814faab4b158ae2b03125ba66b
pivanrios <pipr96@gmail.com>
2024-05-02 16:01:04 -0700

    add components

--- app/page.js
+++ app/page.js
@@ -1,8 +1,16 @@
-import Image from "next/image";
+import DishGallery from "@/components/DishGallery";
+import HomeBanner from "@/components/HomeBanner";
+import RegisterForm from "@/components/RegisterForm";
+import ReviewsGallery from "@/components/ReviewsGallery";
+
 
 export default function Home() {
   return (
     <>
+    <HomeBanner/>
+    <DishGallery/>
+    <RegisterForm/>
+    <ReviewsGallery/>
     </>
   );
 }
