86598bb089c0bf83d09679a245ff38cc3a015f09
pivanrios <pipr96@gmail.com>
2024-05-02 17:42:11 -0700

    add content to banner

--- components/HomeBanner.jsx
+++ components/HomeBanner.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 function HomeBanner() {
   return (
-    <div>HomeBanner</div>
+    <div id="banner" className='container flex flex-col px-10 py-5 bg-slate-400 justify-between'>
+      <h1 className='container flex flex-col'>
+        <b className='text-4xl'>LaCooperativa</b>
+        <span className='text-sm'>Comida casera a tu empresa</span>
+      </h1>
+      <p className=' '>Somos una cooperativa que conecta empleados que no tienen tiempo <br/>
+      para cocinar con companeros que si les da tiempo... </p>
+      <button className='btn w-auto bg-yellow-500 self-end'>Learn more</button>
+      
+    </div>
   )
 }
 
