11aa1c8df46d4d09b81dee135443d874a42700c5
pivanrios <pipr96@gmail.com>
2024-05-03 21:42:53 -0700

    add images

---
+++ public/data.js
@@ -0,0 +1,36 @@
+export const dishes = [
+    {
+        imagen: 'images/lasagna.jpeg',
+        name: "Lasagna",
+        content: "Some content abaout the dish, lorem siponseiom...",
+        price: 9,
+        id:1
+    },
+    {
+        imagen: 'images/rajas.jpeg',
+        name: "Rajas",
+        content: "Some content abaout the dish, lorem siponseiom...",
+        price: 7,
+        id:2
+    },
+    {
+        imagen: 'images/hotcakes.jpeg',
+        name: "Mole",
+        content: "Some content abaout the dish, lorem siponseiom...",
+        price: 8,
+        id:3
+    },{
+        imagen: "",
+        name: "Rajas",
+        content: "Some content abaout the dish, lorem siponseiom...",
+        price: 7,
+        id:4
+    },
+    {
+        imagen: "",
+        name: "Mole",
+        content: "Some content abaout the dish, lorem siponseiom,",
+        price: 8,
+        id:5
+    }
+]
