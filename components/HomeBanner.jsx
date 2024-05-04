da3342d906889f544b5ae1c62f01863c2a5f815d
pivanrios <pipr96@gmail.com>
2024-05-03 21:58:20 -0700

    style add space

--- components/HomeBanner.jsx
+++ components/HomeBanner.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 function HomeBanner() {
   return (
-    <div id="banner" className='container flex flex-col px-10 py-5 bg-slate-400 justify-between'>
-      <h1 className='container flex flex-col'>
+    <div id="banner" className='container flex flex-col px-10 pt-5 py-10 bg-slate-400 justify-between'>
+      <h1 className='container flex flex-col p-2'>
         <b className='text-4xl'>LaCooperativa</b>
-        <span className='text-sm'>Comida casera a tu empresa</span>
+        <span className='text-sm'>Comida casera at your service</span>
       </h1>
-      <p className=' '>Somos una cooperativa que conecta empleados que no tienen tiempo <br/>
+      <p className='px-2 pt-10 pb-10 self-end '>Somos una cooperativa que conecta empleados que no tienen tiempo <br/>
       para cocinar con companeros que si les da tiempo... </p>
-      <button className='btn w-auto bg-yellow-500 self-end'>Learn more</button>
+      <button className='btn w-auto bg-yellow-500 self-end '>Learn more</button>
       
     </div>
   )
