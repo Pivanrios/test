aabd6c8ad7cde8a3b04ad675918ee95ffce6ffec
pivanrios <pipr96@gmail.com>
2024-05-04 21:31:35 -0700

    add dish details page

--- app/platillos/[platillo]/page.js
+++ app/platillos/[platillo]/page.js
@@ -1,8 +1,19 @@
+
 import React from 'react'
 
 function Platillo() {
   return (
-    <div>Platillo</div>
+    <div className='flex flex-row justify-between px-3 md:px-10 bg-yellow-500 '>
+      <button>D</button>
+      <div className='flex flex-col bg-yellow-400 p-10'>
+        <h3 className='text-2xl md:text-4xl font-bold'>Nombre Del Producto</h3>
+        <p>product details</p>
+
+        <p>reviews</p>
+        <button className='btn'>add review</button>
+      </div>
+      <button>L</button>
+    </div>
   )
 }
 
