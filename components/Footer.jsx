6747d72a97d5f96f380f0011fe3ccef73f1e0ec4
pivanrios <pipr96@gmail.com>
2024-05-04 18:28:21 -0700

    create footer

---
+++ components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <section>
+        <div>
+            <h3>Contact us</h3>
+            <ol>
+                <div id='instagra'></div>
+                <div id='whatsapp'></div>
+                <div id='linkedin'></div>
+            </ol>
+            <span>copy right</span>
+        </div>
+    </section>
+  )
+}
+
+export default Footer
